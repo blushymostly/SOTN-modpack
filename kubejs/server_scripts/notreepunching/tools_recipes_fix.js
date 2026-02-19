35d2c4526c4ec983226ab0ea51e15b55fc7922f9
blushy <prolinkcraft@gmail.com>
2026-02-19 22:49:03 +0300

    added strength aptitudes for almost all weapons

--- kubejs/server_scripts/notreepunching/tools_recipes_fix.js
+++ kubejs/server_scripts/notreepunching/tools_recipes_fix.js
@@ -13,13 +13,13 @@ ServerEvents.recipes(event => {
     })
 
   event.shaped('minecraft:wooden_axe', [
-    'PP ',
-    'PS ',
+    ' PP',
+    ' SP',
     ' S '
   ], {
       P: '#minecraft:planks',
       S: 'minecraft:stick'
-    })
+    }).noMirror().noShrink().id('minecraft:wooden_axe')
 
   event.shaped('minecraft:wooden_hoe', [
     'PP ',
@@ -59,13 +59,13 @@ ServerEvents.recipes(event => {
   })
 
   event.shaped('minecraft:stone_axe', [
-    'CC ',
-    'CS ',
+    ' CC',
+    ' SC',
     ' S '
   ], {
       C: 'minecraft:cobblestone',
       S: 'minecraft:stick'
-    })
+    }).noMirror().noShrink()
   
   event.shaped('minecraft:stone_hoe', [
     'CC ',
